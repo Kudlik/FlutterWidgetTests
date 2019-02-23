31004819c4f3fe36d448a980a6b36c5b079e1e04
Kudlik <jaskulski.maciek.spam@gmail.com>
2019-02-23 18:30:57 +0100

    test changes

--- using_tabs/test/widget_test.dart
+++ using_tabs/test/widget_test.dart
@@ -2,31 +2,18 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-
-  testWidgets('searchingForString', (WigdetTester tester) async{
-
-    await tester.pumpWidget(main(
-      MaterialApp
-    ))
-  });
-
-/*testWidgets('clickingOnTabs', (WidgetTester tester) async{
-
-    await tester.tap(find.byIcon(Icons.favorite));
+  testWidgets('finding text', (WidgetTester tester) async {
+    await tester.pumpWidget(Container(
+        child: Center(
+            child: Column(
+                children: <Widget>[Text('First Tab')
+                ],
+            ),
+        ),
+    ),
+    );
     expect(find.text('First Tab'), findsOneWidget);
   });
-
-  testWidgets('secondTab', (WidgetTester tester) async{
-
-    await tester.tap(find.byIcon(Icons.adb));
-    expect(find.text("Second Tab"), findsOneWidget);
-  });
-
-  testWidgets('thirdTab', (WidgetTester tester) async{
-
-    await tester.tap(find.byIcon(Icons.airport_shuttle));
-    expect(find.text("Third Tab"), findsOneWidget);
-  });
-*/
 }
+
 
